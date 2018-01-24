0c21cbad6d45fdbf660a0bb1c982660721c44668
Unknown <gordon.lee.6170@gmail.com>
2018-01-24 15:51:55 -0500

    add project

---
+++ Unity_AD_Test/Assets/Scripts/Main.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Advertisement.Initialize("1681330");
+        Advertisement.Show();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void ShowRewardedVideo() {
+        ShowOptions options = new ShowOptions();
+        options.resultCallback = HandleShowResult;
+        Advertisement.Show("rewardedVideo", options);
+    }
+
+    void HandleShowResult(ShowResult result) {
+        if (result == ShowResult.Finished) {
+            Debug.Log("Video completed - Offer a reward to the player");
+
+        } else if (result == ShowResult.Skipped) {
+            Debug.LogWarning("Video was skipped - Do NOT reward the player");
+
+        } else if (result == ShowResult.Failed) {
+            Debug.LogError("Video failed to show"); 
+        }
+    }
+}
